29581e800851f8084e32a47475e6cedc54169cf8
rocharju <rocharju@169.254.41.151>
2019-12-12 17:49:06 +0100

    wip

--- src/deplacement/Libre.java
+++ src/deplacement/Libre.java
@@ -5,14 +5,23 @@ import java.util.List;
 
 public class Libre extends Deplacement {
 	
+	private List<Deplacement> deplacements;
+	private int cursor;
+	
 	public Libre(List<Deplacement> deplacements) {
 		// TODO Auto-generated constructor stub
+		this.deplacements = deplacements;
+		this.cursor = 0;
 	}
 
 	@Override
 	public Point getNextPoint(Point p) {
 		// TODO Auto-generated method stub
-		return null;
+		if (this.deplacements.get(this.cursor).isDeplacementFini())
+			this.cursor++;
+		if (this.cursor==this.deplacements.size())
+			this.cursor=0;
+		return this.deplacements.get(this.cursor).getNextPoint(p);
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,12 @@ package main;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.List;
 
+import deplacement.Deplacement;
 import deplacement.Direction;
 import deplacement.Horizontale;
+import deplacement.Libre;
 import deplacement.Verticale;
 import model.Balise;
 import model.Satellite;
@@ -23,12 +26,8 @@ public class Main {
 		System.out.println("Nombre de satellite ?");
 		int nbSat = Integer.parseInt(Tools.readKeyboard());
 		
-		Notifier notifier = new Notifier();
-		
-//		System.out.println("Nombre de balises ?");
-//		int nbBalise = Integer.parseInt(Tools.readKeyboard());
-		
 		BaliseFrame app = new BaliseFrame();
+		Notifier notifier = new Notifier();
 		
 		int ecart = GlobaleVariable.sizeX/nbSat;
 		for (int i = 0 ; i < nbSat ; i++) {
@@ -51,15 +50,16 @@ public class Main {
 		balises.add(balise1);
 		app.addBalise(balise1, p);
 		
-//		for (int i = 0 ; i < nbBalise ; i++) {
-//			Point p = new Point(300,GlobaleVariable.sizeY-GlobaleVariable.hauteurMer-5);
-//			Balise balise = new Balise(p, app);
-//			balise.setDep(new Horizontale(15, Direction.Gauche));
-//			balise.addProfondeur(20);
-//			balise.addAllSats(sats);
-//			balises.add(balise);
-//			app.addBalise(balise, p);
-//		}
+		List<Deplacement> deplacements = new ArrayList<Deplacement>();
+		deplacements.add(new Verticale(5,Direction.Bas));
+		deplacements.add(new Horizontale(5,Direction.Droite));
+		
+		p = new Point(200,GlobaleVariable.sizeY-GlobaleVariable.hauteurMer-5);
+		Balise balise2 = new Balise(p, app, notifier);
+		balise2.setDep(new Libre(deplacements));
+		balise2.addProfondeur(20);
+		balises.add(balise2);
+		app.addBalise(balise2, p);
 		
 		for (int i = 0 ; i < sats.size() ; i++) {
 			sats.get(i).start();
