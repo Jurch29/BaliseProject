b379b21ab21b9bdb429d76b325f68e149a3e8078
Jurch29 <julien.rch.29@gmail.com>
2019-12-19 21:15:37 +0100

    correctif mineur

--- src/tools/GlobaleVariable.java
+++ src/tools/GlobaleVariable.java
@@ -3,9 +3,9 @@ package tools;
 public class GlobaleVariable {
 	public static final int vitesseSat = 5;
 	public static final int vitesseBalise = 5;
-	public static int sizeX = 800;
-	public static int sizeY = 600;
-	public static int hauteurMer = 200;
-	public static int vitesseSimulation = 60;
-	public static int difficulteCollecte = 5;
+	public static final int sizeX = 800;
+	public static final int sizeY = 600;
+	public static final int hauteurMer = 200;
+	public static final int vitesseSimulation = 60;
+	public static final int difficulteCollecte = 5;
 }
